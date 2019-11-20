a4a6c8d9586b7844412edd83bce41928e973bb39
datguyrhy <datguyrhy@Branz.localdomain>
2019-11-21 01:48:46 +0800

    added showing off user names

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tweets from 'tweets';
+import User from "user";
+import Post from "tweetpost";
 
 class App extends React.Component {
   render() {
+    console.log(Post);
+var allTweets = Tweets.map(tweet=>{
+  console.log(tweet)
+  return(
+  <div>
+      <p>{tweet.user.name}</p>
+      <p>{tweet.user.screen_name}</p>
+
+  </div>
+  )
+})
+
     return (
       <div>
-        tweets
+        {allTweets}
       </div>
     );
   }
